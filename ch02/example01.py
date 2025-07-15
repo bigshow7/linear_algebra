e5e29e245582252f993108819fe3e2bf974a92a8
bigshow7 <john522130@gmail.com>
2025-07-15 16:20:14 +0900

    modify example01.py
    add checking inv_A error
    remove array because @ makes array automatically
    add numpy.linalg.solve part

--- ch02/example01.py
+++ ch02/example01.py
@@ -16,7 +16,18 @@ A = np.array([
 b = np.array([1,0])
 
 inv_A = inv_mat(A)
-ans = np.array(inv_A @ b)
+if inv_A is None:
+    raise RuntimeError("역행렬 계산 실패: 행렬식이 0이거나 입력 오류")
+
+ans = inv_A @ b     #A의 역행렬과 상수행렬 b를 곱하면 답이 리스트 형태로 출력됨
 x = ans[0]
 y = ans[1]
 print(f"답: x = {x:.2f}, y = {y:.2f}")
+
+'''
+참고)
+Ax = b 형태 연립방정식을 푸는 numpy 함수
+numpy.linalg.solve(a, b)를 쓰면 좀 더 간단히 구할 수 있음
+내부적으로 가우스 소거법 등을 이용해 효율적으로 풀 수 있음
+x, y = solve_linear(A, b)
+'''
