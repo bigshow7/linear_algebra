80d6d5fb6834b05d9b7b859e38150f258da640ad
bigshow7 <john522130@gmail.com>
2025-07-15 15:28:26 +0900

    modify inverse_matrix.py
    Refactor matrix inverse check into a dedicated function
    Includes square matrix validation and handles floating-point precision
    for determinant check using np.isclose().

--- ch02/inverse_matrix.py
+++ ch02/inverse_matrix.py
@@ -2,14 +2,22 @@
 import numpy as np
 
 def inv_mat(matrix):
-    det_A = np.linalg.det(matrix)
-    print(f"입력된 행렬의 행렬식: {det_A}")
+    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1]:
+        print("정방행렬만 지원합니다")
+        return
 
-    if det_A == 0:
+    det = np.linalg.det(matrix)
+    print(f"입력된 행렬의 행렬식: {det:.4f}")       #소수점 4째까지만
+
+    if np.isclose(det, 0):
         print("행렬식이 0이므로 역행렬이 존재하지 않습니다!")
+        return None
 
     else:
-        print(f"입력된 행렬의 역행렬: {np.linalg.inv(matrix)}")
+        inverse_mat = np.linalg.inv(matrix)
+        print(f"입력된 행렬의 역행렬: {inverse_mat}")
+        return inverse_mat  #재사용 시 편리하도록 역행렬 반환
+        
     
 
 matrix_A = np.array([
@@ -29,3 +37,10 @@ matrix_B = np.array([
 
 print(f"행렬 B: {matrix_B}")
 inv_mat(matrix_B)
+
+matrix_C = np.array([
+    [1,2,3],
+    [4,5,6]
+])
+print(f"행렬 C: {matrix_C}")
+inv_mat(matrix_C)
